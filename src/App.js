4ce1c98aa5eb08497fb936c46a31cbc963502cf8
Rafael Damasceno <rafaelsd389@gmail.com>
2023-02-27 21:41:43 -0300

    Criando component LOREM IPSUM

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import LoremIpsum from './components/LoremIpsum';
 import HelloWorld from './HelloWorld';
 
 function App() {
   return (
     <div className="App">
       <HelloWorld />
+      <LoremIpsum />
     </div>
   );
 }
---
+++ src/components/LoremIpsum.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function LoremIpsum() {
+    return (
+        <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vitae ipsa eum sed eius veniam esse? Magnam architecto repellat soluta quae sint, excepturi esse, incidunt veniam iste magni porro, qui eos?
+        </p>
+    )
+}
+
+export default LoremIpsum;
